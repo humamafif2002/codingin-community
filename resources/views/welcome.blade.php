c5a59ac226b036f7a2583b17955d6190381e01e6
imrecker16 <emailmu@email.com>
2026-07-19 22:56:08 +0700

    FIX : FIX DEVELOPMENT CONFIGURATION

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel Docker</title>
+    <title>Laravel Docker Untuk Pemula</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+export default defineConfig(({ mode }) => {
+    // Memuat variabel dari file .env
+    const env = loadEnv(mode, process.cwd(), '');
 
-    ...(env.APP_ENV === 'local' && {
-        server: {
-            host: '0.0.0.0',
-            port: 5173,
-            hmr: {
-                host: 'localhost',
-            },
-            watch: {
-                usePolling: true,
-            },
-        }
-    })
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+
+        ...(env.APP_ENV === 'local' && {
+            server: {
+                host: '0.0.0.0',
+                port: 5173,
+                hmr: {
+                    host: 'localhost',
+                },
+                watch: {
+                    usePolling: true,
+                },
+            }
+        })
+    };
 });
